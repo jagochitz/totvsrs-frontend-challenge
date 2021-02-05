03c41224a6caba118879aec35893e9025b87944d
Rafael Jagochitz <jagoxitz@gmail.com>
2021-02-05 11:06:15 -0300

    criando JS para mostrar tela de login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 console.log("TOTVS RS")
+
+/* mostrar tela de login */
+let entrar = document.querySelectorAll(".entrar")
+
+entrar.addEventListener('click', function(){
+    entrar.classList.add('apresentacao')
+})
