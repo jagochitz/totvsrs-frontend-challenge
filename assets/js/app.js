657b488007a4373d16a25f93ad53c268078b22c3
Rafael Jagochitz <jagoxitz@gmail.com>
2021-02-06 10:44:03 -0300

    Criado form de cadastro e corpo da todo list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,1 @@
 console.log("TOTVS RS")
-
-/* mostrar tela de login */
-let entrar = document.querySelectorAll(".entrar")
-
-entrar.addEventListener('click', function(){
-    entrar.classList.add('apresentacao')
-})
